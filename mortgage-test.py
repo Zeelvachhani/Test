8b2cea66fcfbebd58ad6b92c73fd0794e1b2903b
Zeelvachhani <zeel1292@gmail.com>
2025-07-08 22:31:57 -0700

    Update mortgage-test.py

--- mortgage-test.py
+++ mortgage-test.py
@@ -555,12 +555,12 @@ st.markdown("---", unsafe_allow_html=True)
 st.markdown(
     """
     <div style="text-align: center; font-size: 14px;">
-        ✨ Crafted with care by <strong>Zeel Vachhani</strong> ✨<br><br>
+        ✨ Crafted with care by <strong>Zeel Vachhani</strong> ✨<br>
         <a href="https://www.linkedin.com/in/zeelvachhani" target="_blank" style="text-decoration: none;">
             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" height="18" style="vertical-align: text-bottom; margin-right: 6px;">
             Connect on LinkedIn
         </a>
-        <br><br>
+        <br>
         © 2025 Zeel Vachhani. All rights reserved.<br>
         <em>This tool is for informational purposes only and should not be considered financial advice.</em>
     </div>
